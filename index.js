ccf9a8f3470312e77e03d9efd698b6e686dfc0f2
zangar9805 <zangar9805@gmail.com>
2020-06-21 08:05:14 +0600

    Updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1221287476:AAGs1jEvRyminFH4eRLnr3INdshbz9wDcqU')
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
